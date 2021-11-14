38a62489ad8be252f331b27d4a0cc0ad429e1e71
Kvadroman <kvadrostoplerman@gmail.com>
2021-11-14 11:04:29 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImRich
+//
+//  Created by Ивченко Антон on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
